57ff7d4027e29251d66bf180633a71bd37129443
lens0021 <lorentz0021@gmail.com>
2025-12-20 12:02:33 +0900

    fix: Raise errors from the user script if any

--- dist/main.sh
+++ dist/main.sh
@@ -98,16 +98,16 @@ __status=$?
 script_hash_6="${ret_env_var_get99_v0}"
 # Calculate cache path
 trim__11_v0 "${cache_path_input_4}"
-ret_trim11_v0__12_26="${ret_trim11_v0}"
+ret_trim11_v0__13_28="${ret_trim11_v0}"
 env_var_get__99_v0 "HOME"
 __status=$?
-ret_env_var_get99_v0__13_16="${ret_env_var_get99_v0}"
-amber_cache_path_7="$(if [ "$([ "_${ret_trim11_v0__12_26}" != "_" ]; echo $?)" != 0 ]; then echo "${ret_env_var_get99_v0__13_16}/.cache/amber-script-action"; else echo "${cache_path_input_4}"; fi)"
+ret_env_var_get99_v0__14_18="${ret_env_var_get99_v0}"
+amber_cache_path_7="$(if [ "$([ "_${ret_trim11_v0__13_28}" != "_" ]; echo $?)" != 0 ]; then echo "${ret_env_var_get99_v0__14_18}/.cache/amber-script-action"; else echo "${cache_path_input_4}"; fi)"
 dist_path_8="${amber_cache_path_7}/dist/${script_hash_6}.sh"
 # Build the given script
 file_exists__38_v0 "${dist_path_8}"
-ret_file_exists38_v0__19_4="${ret_file_exists38_v0}"
-if [ "${ret_file_exists38_v0__19_4}" != 0 ]; then
+ret_file_exists38_v0__20_6="${ret_file_exists38_v0}"
+if [ "${ret_file_exists38_v0__20_6}" != 0 ]; then
     echo "::debug::A compiled bash script found. Skip building."
 else
     dir_create__43_v0 "${amber_cache_path_7}/tmp"
@@ -127,3 +127,6 @@ fi
 # Run the compiled script
 bash "${dist_path_8}"
 __status=$?
+if [ "${__status}" != 0 ]; then
+    exit "${__status}"
+fi
