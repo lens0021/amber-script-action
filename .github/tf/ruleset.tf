da665ee042e1f326a89fa9bdded2d7a2c2e63fed
Lens0021 / Leslie <lorentz0021@gmail.com>
2026-02-26 21:02:24 +0900

    ci(tofu): Manage Github settings as code (#105)

---
+++ .github/tf/ruleset.tf
@@ -0,0 +1,59 @@
+import {
+  id = "amber-script-action:6070737"
+  to = github_repository_ruleset.default
+}
+resource "github_repository_ruleset" "default" {
+  name        = "default"
+  repository  = github_repository.this.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH", ]
+      exclude = []
+    }
+  }
+
+  rules {
+    deletion                = true
+    non_fast_forward        = true
+    required_linear_history = true
+    required_signatures     = false
+    update                  = true
+
+    pull_request {
+      dismiss_stale_reviews_on_push     = false
+      require_code_owner_review         = false
+      require_last_push_approval        = false
+      required_approving_review_count   = 0
+      required_review_thread_resolution = false
+    }
+
+    required_status_checks {
+      do_not_enforce_on_create             = false
+      strict_required_status_checks_policy = false
+
+      dynamic "required_check" {
+        for_each = [
+          "biome",
+          "check-dist",
+          "--> Linted: CHECKOV",
+          "--> Linted: GITHUB_ACTIONS",
+          "--> Linted: GITLEAKS",
+          "--> Linted: GIT_MERGE_CONFLICT_MARKERS",
+          "--> Linted: JSCPD",
+          "rumdl",
+          "Test basic script execution (macos-latest)",
+          "Test basic script execution (ubuntu-latest)",
+          "zizmor",
+        ]
+        content {
+          context        = required_check.value
+          integration_id = 15368
+        }
+      }
+    }
+
+  }
+}
